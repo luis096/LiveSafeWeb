d7687f1308b91fe8d5fbeeeec8e824a1b5a6115f
Cholod Estrella <estrellangie19@gmail.com>
2020-04-16 18:44:04 -0300

    cartel confirmar

--- src/componente/Perfil/Configuraciones.js
+++ src/componente/Perfil/Configuraciones.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import Button from 'components/CustomButton/CustomButton.jsx';
-
+import SweetAlert from 'react-bootstrap-sweetalert';
 
 class Configuraciones extends Component {
 
@@ -19,16 +19,46 @@ class Configuraciones extends Component {
 
     reestablecer(){
          let newPassword = this.state.pass;
+         debugger
          firebase.auth().currentUser.updatePassword(newPassword).then(() =>{
+             this.hideAlert()
          }, error=> {
             console.log(error.message);
         },)
         }
+
+    hideAlert() {
+        this.setState({
+            alert: null
+        });
+    }
+        
+    confirmar(){
+        this.setState({
+            alert: (
+                <SweetAlert
+                    warning
+                    style={{display: 'block', marginTop: '-100px', position: 'center'}}
+                    title="¿Estas seguro?"
+                    onConfirm={()=>this.reestablecer()}
+                    onCancel={()=>this.hideAlert()}
+                    confirmBtnBsStyle="info"
+                    cancelBtnBsStyle="danger"
+                    confirmBtnText="Si, estoy seguro"
+                    cancelBtnText="Cancelar"
+                    showCancel
+                >
+                    ¿Esta seguro de que desea cambiar la conytraseña?
+                </SweetAlert>
+            )
+        });
+    }
     
 
     render() {
         return (
             <div className="col-12">
+                {this.state.alert}
                 <div className="row card">
                     <div className="card-body">
                         <legend><h3 className="row"><i className="pe-7s-tools"/> Configuraciones</h3></legend>
@@ -42,7 +72,7 @@ class Configuraciones extends Component {
                             </div>
                             <div className="col-md-6">
                                 <Button bsStyle="default"  fill wd onClick={()=> {
-                                    this.reestablecer();
+                                    this.confirmar();
                                 }}>Reestablecer</Button>
                             </div>
                                 
